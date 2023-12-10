cd0b9b627092de312149169c462b1ac62355df59
Krzysztof Heppner <Krzysztof.Heppner1@gmail.com>
2023-12-10 22:07:43 +0100

    koniec

--- BlackBoxGame/BlackBoxGame.cpp
+++ BlackBoxGame/BlackBoxGame.cpp
@@ -17,12 +17,12 @@ const char znaki[17] = { (char)201, (char)187, (char)200, (char)188, (char)186, 
                          (char)218, (char)191, (char)192, (char)217, (char)179, (char)196,
                          (char)197, (char)194, (char)195, (char)193, (char)180};
 /* 0:w 1:s 2:a 3:d 4:q 5:u 6:r 7:space 8:o 9:k 10:p 11:h 12:N 13:M*/
-const char keyType[14] = { 'W', 'S', 'A', 'D', 'Q', 'U', 'R', ' ', 'O', 'K', 'P', 'H', 'N', 'M'};
+const char keyType[15] = { 'W', 'S', 'A', 'D', 'Q', 'U', 'R', ' ', 'O', 'K', 'P', 'H', 'N', 'M', 'L'};
 const int rozmiarXMax = 110;
 const int rozmiarYMax = 55;
 
 void WyswietlPlansze(char key, char**tab, int aktWiel, int rozmiarXAkt, int rozmiarYAkt, int ods, 
-    int pozycja[2], char**tabColor, bool czyPokazac, int ileAtomow, int lokalizacja[16], char** tabNapisy);
+    int pozycja[2], char**tabColor, bool czyPokazac, int ileAtomow, int lokalizacja[16], char** tabNapisy, int zaznaczone[16], int zaznaczonych);
 
 void Losuj(int lokalizacja[16], int ileAtomow, int aktWiel);
 
@@ -42,7 +42,7 @@ int main() {
             tab[x][y] = ' ';
         }
     }
-
+    
     char** tabColor = new char* [rozmiarXMax];
     for (int i = 0; i < rozmiarXMax; ++i)
     {
@@ -80,11 +80,32 @@ int main() {
     {
         lokalizacja[i] = -5;
     }
+    int zaznaczonych = 0;
+    int zaznaczone[16]{};
+    for (int i = 0; i < 16; i++)
+    {
+        zaznaczone[i] = -4;
+    }
     char key = '-';
     char kierunek = '-';
     bool czyPokazac = false;
-    
+
+    int wynik = 0;
     int kolejnyNumer = 0;
+
+    char ostatnie[50];
+    for (int i = 0; i < 10; i++)
+    {
+        ostatnie[i] = '|';
+    }
+    int ileOstatnich = 0;
+    int ostatnieLok[100];
+    for (int i = 0; i < 50; i++)
+    {
+        ostatnieLok[i*2] = '-9';
+        ostatnieLok[i*2+1] = '-9';
+    }
+
 
     srand((unsigned int)time(NULL));
     #pragma endregion
@@ -102,7 +123,7 @@ int main() {
     rozmiarYAkt = ods * aktWiel;
     rozmiarXAkt = rozmiarYAkt * 2;
 
-    WyswietlPlansze(NULL, tab, aktWiel, rozmiarXAkt, rozmiarYAkt, ods, pozycja, tabColor, czyPokazac, ileAtomow, lokalizacja, tabNapisy);
+    WyswietlPlansze(NULL, tab, aktWiel, rozmiarXAkt, rozmiarYAkt, ods, pozycja, tabColor, czyPokazac, ileAtomow, lokalizacja, tabNapisy, zaznaczone, zaznaczonych);
 
     while (true)
     {
@@ -110,7 +131,7 @@ int main() {
         if ((int)key > 91){
             key = (char)((int)key - 32);
         }
-        for (int i = 0; i < 14; i++)
+        for (int i = 0; i < 15; i++)
         {
             if (key == keyType[i])
             {
@@ -129,6 +150,8 @@ int main() {
                                 }
                             }
                             kolejnyNumer = 0;
+                            zaznaczonych = 0;
+                            ileOstatnich = 0;
                         }break;
                     case 'M': // mniej
                         if ((aktWiel != 7) && (pozycja[1] < aktWiel - 1) && (pozycja[0] < aktWiel - 1)){ 
@@ -143,6 +166,8 @@ int main() {
                                 }
                             }
                             kolejnyNumer = 0;
+                            zaznaczonych = 0;
+                            ileOstatnich = 0;
                         }break;
                     case 'W':
                         if (pozycja[0] != 0) { pozycja[0]--; } break;
@@ -152,9 +177,23 @@ int main() {
                         if (pozycja[1] != 0) { pozycja[1]--; } break;
                     case 'D':
                         if (pozycja[1] < aktWiel - 1) { pozycja[1]++; } break;
+                    case 'Q':
+                        cin.get();
+                        Start(&aktWiel, &ileAtomow); 
+                        Losuj(lokalizacja, ileAtomow, aktWiel); 
+                        for (int y = 0; y < rozmiarYMax; ++y) {
+                            for (int x = 0; x < rozmiarXMax; ++x) {
+                                tabNapisy[x][y] = ' ';
+                            }
+                        }
+                        kolejnyNumer = 0;
+                        zaznaczonych = 0;
+                        ileOstatnich = 0; break;
                     case 'H':
-                        czyPokazac = !czyPokazac; break;
-                    case 'K':
+                        czyPokazac = true; break;
+                    case 'P':
+                        czyPokazac = true; break;
+                    case 'L':
                         Losuj(lokalizacja, ileAtomow, aktWiel); 
                         for (int y = 0; y < rozmiarYMax; ++y) {
                             for (int x = 0; x < rozmiarXMax; ++x) {
@@ -163,8 +202,70 @@ int main() {
                         }
                         kolejnyNumer = 0;
                         break;
-                    case 'Q':
-                        goto koniec; break;
+                    case 'K':
+                        for (int kk = 0; kk < zaznaczonych; kk++)
+                        {
+                            tabNapisy[zaznaczone[kk*2] * ods * 2 + ods][zaznaczone[kk * 2+1] * ods + ods / 2] = 'X';
+                        }
+                        for (int ii = 0; ii < zaznaczonych; ii++)
+                        {
+                            for (int jj = 0; jj < ileAtomow; jj++)
+                            {
+                                if (zaznaczone[ii*2]==lokalizacja[jj*2] && zaznaczone[ii * 2+1] == lokalizacja[jj * 2+1])
+                                {
+                                    tabNapisy[zaznaczone[ii * 2] * ods * 2 + ods][zaznaczone[ii * 2+1] * ods + ods / 2] = 'O';
+                                    wynik++;
+                                }
+                            }
+                        }
+                        czyPokazac = true;
+                        goto pomin; break;
+                    case 'O':
+                        if (zaznaczonych<ileAtomow)
+                        {
+                            for (int j = zaznaczonych; j < 50; j++)
+                            {
+                                ostatnie[j] = '|';
+                            }
+                            for (int k = 0; k < zaznaczonych; k++)
+                            {
+                                if ((zaznaczone[k * 2] == pozycja[1]) && (zaznaczone[k * 2 + 1] == pozycja[0]))
+                                {
+                                    goto pomin;
+                                }
+                            }
+
+                            zaznaczone[zaznaczonych * 2] = pozycja[1];
+                            zaznaczone[zaznaczonych * 2 + 1] = pozycja[0];
+                            zaznaczonych++;
+
+                            ostatnie[ileOstatnich] = 'O';
+                            ostatnieLok[ileOstatnich * 2] = pozycja[1];
+                            ostatnieLok[ileOstatnich * 2 + 1] = pozycja[0];
+                            ileOstatnich++;
+                        }
+                        break;
+                    case 'U':
+                        if (ileOstatnich > 0)
+                        {
+                            if (ostatnie[ileOstatnich - 1] == 'O')
+                            {
+                                if (zaznaczonych > 0)
+                                {
+                                    zaznaczonych--;
+                                }
+                            }
+                            ileOstatnich--;
+                        }break;
+                    case 'R':
+                        if (ileOstatnich <= zaznaczonych)
+                        {
+                            if (ostatnie[ileOstatnich] == 'O')
+                            {
+                                zaznaczonych++;
+                                ileOstatnich++;
+                            }
+                        }break;
                     case ' ':
                         if ((pozycja[0] == 0) && !((pozycja[1] == 0) || (pozycja[1] == aktWiel-1))) {
                             kierunek = 'd';
@@ -185,8 +286,30 @@ int main() {
                     default:
                         break;
                 }
+                pomin:
+                if (key == 'H') 
+                {
+                    for (int y = 0; y < rozmiarYMax; y++) {
+                        for (int x = 0; x < rozmiarXMax; x++) {
+                            tab[x][y] = ' ';
+                        }
+                    }
+                    for (int y = 0; y < rozmiarYMax; ++y) {
+                        for (int x = 0; x < rozmiarXMax; ++x) {
+                            tabColor[x][y] = (short)0;
+                        }
+                    }
+                    rozmiarXAkt = rozmiarXMax - 1;
+                    rozmiarYAkt = rozmiarYMax - 1;
+                    ods = (rozmiarYAkt + 1) / (aktWiel + 1);
+                    rozmiarYAkt = ods * aktWiel;
+                    rozmiarXAkt = rozmiarYAkt * 2;
+                    WyswietlPlansze(keyType[i], tab, aktWiel, rozmiarXAkt, rozmiarYAkt, ods, pozycja, tabColor, czyPokazac, ileAtomow, lokalizacja, tabNapisy, zaznaczone, zaznaczonych);
 
-
+                    clock_t star = clock();
+                    while ((clock() - star) / CLOCKS_PER_SEC < 1) {}
+                    czyPokazac = false;
+                }
 
                 for (int y = 0; y < rozmiarYMax; y++) {
                     for (int x = 0; x < rozmiarXMax; x++) {
@@ -203,19 +326,29 @@ int main() {
                 ods = (rozmiarYAkt + 1) / (aktWiel + 1);
                 rozmiarYAkt = ods * aktWiel;
                 rozmiarXAkt = rozmiarYAkt * 2;
-                WyswietlPlansze(keyType[i], tab, aktWiel, rozmiarXAkt, rozmiarYAkt, ods, pozycja, tabColor, czyPokazac, ileAtomow, lokalizacja, tabNapisy);
+                WyswietlPlansze(keyType[i], tab, aktWiel, rozmiarXAkt, rozmiarYAkt, ods, pozycja, tabColor, czyPokazac, ileAtomow, lokalizacja, tabNapisy, zaznaczone, zaznaczonych);
+                if (key == 'P' || key == 'K')
+                {
+                    goto koniec;
+                }
             }
 
         }
     }
     koniec:
+    if (key == 'K')
+    {
+        cout << "Zdobyte punkty: " << wynik;
+    }
 
     for (int i = 0; i < rozmiarXMax; ++i) {
         delete[] tab[i];
         delete[] tabColor[i];
+        delete[] tabNapisy[i];
     }
     delete[] tab;
     delete[] tabColor;
+    delete[] tabNapisy;
 
     return 0;
 }
@@ -225,6 +358,10 @@ void Losuj(int lokalizacja[16], int ileAtomow, int aktWiel)
     bool czyDobry = true;
     int tempX = -5, tempY = -5;
     tempX = -5, tempY = -5;
+    for (int k = 0; k < 16; k++)
+    {
+        lokalizacja[k] = -2;
+    }
     for (int i = 0; i < ileAtomow; i++)
     {
         czyDobry = true;
@@ -248,6 +385,7 @@ void Losuj(int lokalizacja[16], int ileAtomow, int aktWiel)
 
 void Start(int* aktWiel, int* ileAtomow)
 {
+    system("cls");
     char znak = '|';
     cout << "************************************************************\n";
     cout << "*                                                          *\n";
@@ -281,6 +419,7 @@ void Start(int* aktWiel, int* ileAtomow)
     default:
         cout << "z" << (char)136 << "a liczba - ustawiono 1 opcje - wcisnij dowolny klawisz aby kontynuowa" << (char)134;
         cin.get();
+        cin.get();
         *aktWiel = 5+2;
         *ileAtomow = 3;
         break;
@@ -290,7 +429,7 @@ void Start(int* aktWiel, int* ileAtomow)
 
 
 void WyswietlPlansze(char key, char**tab, int aktWiel,  int rozmiarXAkt, int rozmiarYAkt, int ods, 
-    int pozycja[2], char**tabColor, bool czyPokazac, int ileAtomow, int lokalizacja[16], char** tabNapisy)
+    int pozycja[2], char**tabColor, bool czyPokazac, int ileAtomow, int lokalizacja[16], char** tabNapisy, int zaznaczone[16], int zaznaczonych)
 {
     #pragma region pusta plansza
     int pozX = 0;
@@ -373,6 +512,18 @@ void WyswietlPlansze(char key, char**tab, int aktWiel,  int rozmiarXAkt, int roz
     }
 
 
+    //zaznaczone
+    for (int i = 0; i < zaznaczonych; i++)
+    {
+        for (int y = zaznaczone[i * 2 + 1] * ods + 1; y < zaznaczone[i * 2 + 1] * ods + ods; y++)
+        {
+            for (int x = zaznaczone[i * 2] * ods * 2 + 1; (x < zaznaczone[i * 2] * 2 * ods + 2 * ods) && x < rozmiarXMax; x++)
+            {
+                tab[x][y] = (char)219;
+                tabColor[x][y] = 'N';
+            }
+        }
+    }
     //atomy
     if (czyPokazac)
     {
@@ -443,18 +594,22 @@ void WyswietlPlansze(char key, char**tab, int aktWiel,  int rozmiarXAkt, int roz
                     cout << G << tab[x][y]; break;
                 case 'B':
                     cout << B << tab[x][y]; break;
+                case 'N':
+                    cout << N << tab[x][y]; break;
                 default:
                     cout << R << tab[x][y]; break;
             }
         }
         switch (y)
         {
+        case 2:
+            cout << "     Atom" << (char)162 << "w: " << ileAtomow; break;
         case 3:
             cout << "     ************ Menu ************"; break;
         case 5:
             cout << "     *  ruch kursora: w,s,a,d     *"; break;
         case 6:
-            cout << "     *  wyj" << (char)152 << (char)134 << "ie: q                *"; break;
+            cout << "     *  wyj" << (char)152 << (char)134 << "ie do menu: q        *"; break;
         case 7:
             cout << "     *  undo: u                   *"; break;
         case 8:
@@ -469,7 +624,13 @@ void WyswietlPlansze(char key, char**tab, int aktWiel,  int rozmiarXAkt, int roz
             cout << "     *  poddaj si" << (char)169 << ": p             *"; break;
         case 13:
             cout << "     *  pomoc (wy" << (char)152 << "wietl atomy): h *"; break;
+        case 14:
+            cout << "     *  losowanie atom" << (char)162 << "w: l       *"; break;
         case 15:
+            cout << "     *  powi" << (char)169 << "kszenie planszy: n   *"; break;
+        case 16:
+            cout << "     *  pomniejszenie planszy: m  *"; break;
+        case 18:
             cout << "     ************ Menu ************"; break;
         default:
             break;
@@ -477,7 +638,10 @@ void WyswietlPlansze(char key, char**tab, int aktWiel,  int rozmiarXAkt, int roz
 
         cout << "\n";
     }
-    cout << "Podaj komendy: ";
+    if (key != 'K')
+    {
+        cout << "Podaj komendy: ";
+    }
 
 }
 
@@ -756,5 +920,5 @@ void Strzal(int* aktWiel, int* ileAtomow, int lokalizacja[16], char** tabNapisy,
         }
     }
     przerwij:
-    cout << "cos";
+    cout << "";
 }
